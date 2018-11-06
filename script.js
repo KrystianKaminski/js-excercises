fbd930cdf1d69a43e659e30fb1062315c3f2d781
Krystian Kaminski <krystiankaminski07@gmail.com>
2018-11-06 12:14:28 +0100

    my own find

--- script.js
+++ script.js
@@ -1,21 +1,19 @@
 const numbers = [1, 2, 3, 4, 5, 6]
 
-Array.prototype.myOwnForFilter = function(func) {
+Array.prototype.myOwnForFind = function(func) {
     const newArray = []
 
     for (let i = 0; i < this.length; i++) {
       if (func(this[i], i, this)) {
-          newArray.push(this[i])
+        return this[i]
       }
     }
-    return newArray
+    return undefined
 
 }
 
+const result = numbers.find( e => e % 2 === 0 )
+console.log(result)
 
-
-const result = numbers.filter((e) => e % 2)
-const result1 = numbers.myOwnForFilter(e =>  e % 2 === 0)
-
-console.log(result) // [1,3,5]
-console.log(result1) // [2,4,6]
+const result1 = numbers.myOwnForFind( e => e % 2 === 0)
+console.log(result1)
