7f3f9bf620b8285beb70963be832f18a179fe14a
Krystian Kaminski <krystiankaminski07@gmail.com>
2018-11-06 11:58:18 +0100

    my own map fn

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
 const numbers = [1, 2, 3, 4, 5, 6]
 
-Array.prototype.myOwnForEach = function(func) {
+Array.prototype.myOwnForMap = function(func) {
+    const newArray = []
 
     for (let i = 0; i < this.length; i++) {
-        func (this[i], i, this)
+        newArray.push(func(this[i], i, this))
     }
-    
+    return newArray
 
 }
 
-// myOwnForEach(numbers,
-//      function (e) {
-//         console.log(e, this)
-// }, {whatever: 'ever'})
+
+
+const result = numbers.myOwnForMap( e => 2 * e)
+
+console.log(result)
